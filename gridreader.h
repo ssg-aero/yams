d9150f89a2494212993365ceaaeec17dc57247b0
szebos <sebastien.newe@gmail.com>
2021-06-25 12:48:53 +0200

    add grid reader

---
+++ gridreader.h
@@ -0,0 +1,66 @@
+#pragma once
+#include <vtkNew.h>
+#include <vtkXMLStructuredGridReader.h>
+#include <vtkStructuredGrid.h>
+#include <vtkPoints.h>
+#include <datastorage.h>
+namespace quiss
+{
+    template<typename T>
+    auto read_vtk_grid(const char *fname)
+    {
+        vtkNew<vtkXMLStructuredGridReader> reader;
+        reader->SetFileName(fname);
+        reader->Update();
+
+        auto sgrid = reader->GetOutput();
+        auto points= sgrid->GetPoints();
+        auto dims  =sgrid->GetDimensions();
+        size_t ni = dims[0];
+        size_t nj = dims[1];
+
+        MeridionalGrid<T> g(ni,nj);
+
+        vtkIdType id {};
+        for(size_t j {} ; j < nj ; j++)
+        {
+            for(size_t i {} ; i < ni ; i++)
+            {
+                auto pt = points->GetPoint(id);
+                g(i,j).x = pt[0];
+                g(i,j).y = pt[1];
+                id++;
+            }
+        }
+        return g;
+
+    }
+
+    auto read_vtk_grid(auto &g, const char *fname)
+    {
+        vtkNew<vtkXMLStructuredGridReader> reader;
+        reader->SetFileName(fname);
+        reader->Update();
+
+        auto sgrid = reader->GetOutput();
+        auto points= sgrid->GetPoints();
+        auto dims  =sgrid->GetDimensions();
+        size_t ni = dims[0];
+        size_t nj = dims[1];
+
+        g.resize(ni,nj);
+
+        vtkIdType id {};
+        for(size_t j {} ; j < nj ; j++)
+        {
+            for(size_t i {} ; i < ni ; i++)
+            {
+                auto pt = points->GetPoint(id);
+                g(i,j).x = pt[0];
+                g(i,j).y = pt[1];
+                id++;
+            }
+        }
+        
+    }
+}
