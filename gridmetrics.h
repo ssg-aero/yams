7cb79bf1b63cc6b669afaec3aeca6eb0d7523fe2
szebos <sebastien.newe@gmail.com>
2021-06-29 09:40:48 +0200

    remove useless templates

--- gridmetrics.h
+++ gridmetrics.h
@@ -23,15 +23,15 @@ namespace quiss
         }
     }
 
-    template <typename T>
+
     auto fz = [](const auto &gp) { return gp.x; };
-    template <typename T>
+
     auto fr = [](const auto &gp) { return gp.y; };
-    template <typename T>
+
     auto fm = [](const auto &gp) { return gp.m; };
-    template <typename T>
+    
     auto fl = [](const auto &gp) { return gp.l; };
-    template <typename T>
+    
     auto fphi = [](const auto &gp) { return gp.phi; };
 
     template <typename T>
@@ -46,11 +46,11 @@ namespace quiss
             for (auto j = 0; j < nj; j++)
             {
                 // auto gam_ = -PI / 2 + PI * j / (nj - 1.);
-                drqdm = D1_O2_i(g, i, j, fr<T>, fm<T>);
-                dzqdm = D1_O2_i(g, i, j, fz<T>, fm<T>);
+                drqdm = D1_O2_i(g, i, j, fr, fm);
+                dzqdm = D1_O2_i(g, i, j, fz, fm);
                 g(i,j).phi = atan2(drqdm, dzqdm); // Stream line angle
-                drqdl = D1_O2_j(g, i, j, fr<T>, fl<T>);
-                dzqdl = D1_O2_j(g, i, j, fz<T>, fl<T>);
+                drqdl = D1_O2_j(g, i, j, fr, fl);
+                dzqdl = D1_O2_j(g, i, j, fz, fl);
                 g(i,j).gam = atan2(dzqdl, drqdl); // Span line angle
             }
         }
@@ -66,7 +66,7 @@ namespace quiss
         {
             for (auto j = 0; j < nj; j++)
             {
-                g(i,j).cur = D1_O2_i(g, i, j, fphi<T>, fm<T>);// TODO check why Aungier put -DphiDm
+                g(i,j).cur = D1_O2_i(g, i, j, fphi, fm);// TODO check why Aungier put -DphiDm
             }
         }
         for (auto j = 0; j < nj; j++)
@@ -77,7 +77,7 @@ namespace quiss
     }
 
     template <typename T>
-    inline auto compute_metrics(MeridionalGrid<T> &g)
+    inline auto compute_geom_values(MeridionalGrid<T> &g)
     {
         compute_abscissas(g);
         compute_angles(g);
@@ -98,15 +98,15 @@ namespace quiss
             for (auto j = 0; j < nj; j++)
             {
                 // auto gam_ = -PI / 2 + PI * j / (nj - 1.);
-                // drqdm = D1_O2_i(g, i, j, fr<T>, fm<T>);
-                // dzqdm = D1_O2_i(g, i, j, fz<T>, fm<T>);
-                drqdm = D1_O2_dx1(g,g_metrics, i, j, d_ksi, d_eth, fr<T>);
-                dzqdm = D1_O2_dx1(g,g_metrics, i, j, d_ksi, d_eth, fz<T>);
+                // drqdm = D1_O2_i(g, i, j, fr, fm);
+                // dzqdm = D1_O2_i(g, i, j, fz, fm);
+                drqdm = D1_O2_dx1(g,g_metrics, i, j, d_ksi, d_eth, fr);
+                dzqdm = D1_O2_dx1(g,g_metrics, i, j, d_ksi, d_eth, fz);
                 g(i,j).phi = atan2(drqdm, dzqdm); // Stream line angle
-                // drqdl = D1_O2_j(g, i, j, fr<T>, fl<T>);
-                // dzqdl = D1_O2_j(g, i, j, fz<T>, fl<T>);
-                drqdl = D1_O2_dx2(g,g_metrics, i, j, d_ksi, d_eth, fr<T>);
-                dzqdl = D1_O2_dx2(g,g_metrics, i, j, d_ksi, d_eth, fz<T>);
+                // drqdl = D1_O2_j(g, i, j, fr, fl);
+                // dzqdl = D1_O2_j(g, i, j, fz, fl);
+                drqdl = D1_O2_dx2(g,g_metrics, i, j, d_ksi, d_eth, fr);
+                dzqdl = D1_O2_dx2(g,g_metrics, i, j, d_ksi, d_eth, fz);
                 g(i,j).gam = atan2(dzqdl, drqdl); // Span line angle
                 g(i,j).cgp = std::cos(g(i,j).gam+g(i,j).phi);
                 g(i,j).sgp = std::sin(g(i,j).gam+g(i,j).phi);
@@ -126,8 +126,8 @@ namespace quiss
         {
             for (auto j = 0; j < nj; j++)
             {
-                // g(i,j).cur = D1_O2_i(g, i, j, fphi<T>, fm<T>);// TODO check why Aungier put -DphiDm
-                g(i,j).cur = D1_O2_dx1(g,g_metrics, i, j, d_ksi, d_eth, fphi<T>);// TODO check why Aungier put -DphiDm
+                // g(i,j).cur = D1_O2_i(g, i, j, fphi, fm);// TODO check why Aungier put -DphiDm
+                g(i,j).cur = D1_O2_dx1(g,g_metrics, i, j, d_ksi, d_eth, fphi);// TODO check why Aungier put -DphiDm
             }
         }
         for (auto j = 0; j < nj; j++) // extrapolate on bounds
