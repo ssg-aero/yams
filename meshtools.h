8fd9ab4c0a2e76483c81443775edfc0c67ef2f31
szebos <sebastien.newe@gmail.com>
2022-05-26 16:30:47 +0200

    improve meshing

--- meshtools.h
+++ meshtools.h
@@ -4,6 +4,7 @@
 #include <gbs/curves>
 #include <gbs/bscinterp.h>
 #include <gbs-mesh/tfi.h>
+#include <gbs-mesh/smoothing.h>
 namespace yams
 {
     template <typename T>
@@ -53,6 +54,15 @@ namespace yams
 
         return gbs::tfi_mesh_2d<T, 2, P, Q, true>(iso_ksi, iso_eth, ksi_i, eth_j, n_iso_ksi, n_iso_eth);
     }
+    template <typename T>
+    auto mesh_channel(const crv_vector<T> &iso_ksi, const crv_vector<T> &iso_eth, const std::vector<std::vector<T>> &ksi_i, const std::vector<std::vector<T>> &eth_j, size_t n_iso_ksi, size_t n_iso_eth)
+    {
+        const size_t dim = 2;
+        const size_t P = 1;
+        const size_t Q = 1;
+
+        return gbs::tfi_mesh_2d<T, dim, P, Q, true>(iso_ksi, iso_eth, ksi_i, eth_j, n_iso_ksi, n_iso_eth);
+    }
     /**
      * @brief 
      * 
@@ -72,6 +82,69 @@ namespace yams
 
         return mesh_channel(iso_eth, iso_ksi, eth_j, ksi_i, n_iso_eth, n_iso_ksi);
     }
+/**
+ * @brief Compute channel mesh 
+ * 
+ * @tparam T 
+ * @param master_streams 
+ * @param hard_points    : blades points on master stream
+ * @param n_streams      : stream lines number
+ * @param n_spans        : indicative span number
+ * @param tol            : tolerance
+ * @param max_deg        : degree for interpolation between hard points in span direction.
+ * @return auto 
+ */
+    template <typename T>
+    auto mesh_channel(const crv_vector<T> &master_streams, const std::vector<gbs::points_vector<T,2>> &hard_points, size_t n_streams, size_t n_spans, T tol, size_t max_deg = 3)
+    // auto mesh_channel(const crv_vector<T> &master_streams, const std::vector<std::vector<std::array<T,2>>> &hard_points, size_t n_streams, size_t n_spans, T tol, size_t max_deg = 3)
+    {
+
+        auto nc_iso_eth = master_streams.size();
+        if(nc_iso_eth != hard_points.size())
+        {
+            throw std::invalid_argument("incompatible size.");
+        }
+        // Project hard points on curve
+        std::vector<std::vector<T>> eth_j(nc_iso_eth);
+        auto hard_points_it = hard_points.begin();
+        auto eth_j_it       = eth_j.begin();
+        for(const auto &eth_crv : master_streams)
+        {
+            for(const auto &pt : *hard_points_it)
+            {
+                auto [u,d] = gbs::extrema_curve_point(*eth_crv, pt, tol);
+                if(d>tol)
+                {
+                    throw std::invalid_argument("Hard point too far from curve.");
+                }
+                eth_j_it->push_back(u);
+            }
+            std::advance(hard_points_it,1);
+            std::advance(eth_j_it,1);
+        }
+        // Build transverse curve set
+        auto nc_iso_ksi = hard_points.front().size();
+        crv_vector<T> iso_ksi(nc_iso_ksi);
+        std::vector<std::vector<T>> ksi_i(nc_iso_ksi);
+        for(size_t i{} ; i < nc_iso_ksi; i++)
+        {
+            gbs::points_vector<T,2> pts(nc_iso_eth);
+            for(size_t j{} ; j < nc_iso_eth; j++)
+            {
+                if(nc_iso_ksi != hard_points[j].size())
+                {
+                    throw std::invalid_argument("incompatible size.");
+                }
+                pts[j] = hard_points[j][i];
+            }
+            auto u = gbs::curve_parametrization(pts, gbs::KnotsCalcMode::CHORD_LENGTH);
+            auto deg = std::min(max_deg,nc_iso_eth-1);
+            iso_ksi[i] = std::make_shared<gbs::BSCurve<T,2>>( gbs::interpolate(pts, u ,deg) );
+            ksi_i[i]   = u;
+        }
+
+        return mesh_channel(master_streams, iso_ksi, ksi_i, eth_j, n_spans, n_streams);
+    }
 
     template <typename T>
     auto mesh_channel(const crv_vector<T> &iso_eth, const std::vector<T> &ksi_i, const std::vector<T> &eth_j, size_t n_iso_ksi, size_t n_iso_eth, size_t max_deg = 3)
@@ -81,5 +154,36 @@ namespace yams
 
         return mesh_channel(iso_eth, iso_ksi, eth_j, ksi_i, n_iso_eth, n_iso_ksi);
     }
+/**
+ * @brief Smooth mesh blocs
+ * 
+ * @tparam T 
+ * @param pts             : mesh points
+ * @param n_streams       : number of stream
+ * @param n_span_per_bloc : blocs span count
+ * @param max_it          : max smoothing iteration
+ * @param tol             : tolerance eon grid
+ * @return auto 
+ */
+    template <typename T>
+    auto smooth_mesh(gbs::points_vector<T,2> &pts, size_t n_streams, const std::vector<size_t> &n_span_per_bloc, size_t max_it=100, T tol =1e-5)
+    {
+        std::vector<size_t> block_indices(n_span_per_bloc.size()+1);
+        block_indices[0] = 0;
+        std:transform(
+            n_span_per_bloc.begin(), n_span_per_bloc.end(),
+            block_indices.begin(),
+            std::next(block_indices.begin()),
+            []( auto nvi_, auto i_prev ){return i_prev + nvi_-1;}
+        );
+        auto n_spans = pts.size() / n_streams;
+        for( size_t i{1}; i < block_indices.size(); i++)
+        {
+            auto [it, err_max] = gbs::elliptic_structured_smoothing(pts,n_spans,0, n_streams-1,block_indices[i-1],block_indices[i],max_it, tol);
+            printf("iterations : %i, error max: %.3e\n", int(it), err_max);
+        }
+        return pts;
+    }
+
 
 }
--- tests/tests_mesh.cpp
+++ tests/tests_mesh.cpp
@@ -100,3 +100,84 @@ TEST(tests_mesh, channel)
         );
     }
 }
+
+TEST(tests_mesh, channel_arbitrary)
+{
+    size_t p = 3;
+    using T = double;
+    using namespace gbs;
+    // Set 2 curves with diferent parametrization
+    points_vector<T,2> crv1_pts{
+        {0.00045, 0.00000}, // i0
+        {0.00601, 0.00000},
+        {0.02664, 0.01632},
+        {0.03090, 0.02746},
+        {0.03591, 0.03414},
+        {0.03900, 0.03600}, //i1
+        {0.05082, 0.04138},
+        {0.06000, 0.04550}, //i2
+        {0.06557, 0.04677},
+        {0.07500, 0.04700}, //i3
+        {0.07999, 0.04700},
+        {0.10500, 0.04700}, //i4
+        {0.11014, 0.04699},
+        {0.13825, 0.04493},
+        {0.16003, 0.03984}  // i5
+    };
+    auto crv1 = interpolate(
+        crv1_pts ,   
+        p,
+        KnotsCalcMode::CHORD_LENGTH
+    );
+
+    points_vector<T,2> crv2_pts{
+        {0.00010, 0.08550}, // i0
+        {0.03900, 0.08550}, // i1
+        {0.06000, 0.08550}, // i2
+        {0.08000, 0.08550}, // i3
+        {0.11000, 0.08550}, // i4
+        {0.16000, 0.08550}  // i5
+    };
+    auto crv2 = interpolate(
+        crv2_pts ,   
+        p,
+        KnotsCalcMode::CHORD_LENGTH
+    );
+    // set hard points
+    std::vector<size_t> i_crv_1 {0, 5, 7, 9, 11, crv1_pts.size() - 1 };
+    std::vector<size_t> i_crv_2 {0, 1, 2, 3, 4 , crv2_pts.size() - 1};
+    std::vector<points_vector<T,2>> hard_points(2);
+    auto n_hard_pts = i_crv_1.size();
+    for(size_t i{}; i < n_hard_pts; i++)
+    {
+        hard_points[0].push_back(crv1_pts[i_crv_1[i]]);
+        hard_points[1].push_back(crv2_pts[i_crv_2[i]]);
+    }
+    //
+    size_t nu{120};
+    size_t nv{21};
+    // size_t nu{40};
+    // size_t nv{7};
+    auto [pts, ni, nj, n_iso_eth, n_iso_ksi] = yams::mesh_channel<T>(
+        {
+            std::make_shared<BSCurve<T,2>>(crv1),
+            std::make_shared<BSCurve<T,2>>(crv2)
+        },
+        hard_points,
+        nv,
+        nu,
+        1e-6
+    );
+
+    yams::smooth_mesh(pts,nj, n_iso_eth);
+
+    if(PLOT_ON)
+    {
+        auto sgrid_actor = gbs::make_structuredgrid_actor(pts, ni, nj);
+        gbs::plot(
+            sgrid_actor,
+            crv1,
+            crv2
+        );
+    }
+}
